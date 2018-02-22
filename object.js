0f36eab01f37c8a740433ba90201b1b904f76504
Bénédicte Pignol <bpignol@users.noreply.github.com>
2018-02-22 13:57:18 +0100

    update object.js comments about perlin noise

--- object.js
+++ object.js
@@ -7,17 +7,29 @@ function object(x, y, w){ // creating an object
   var offset = 0; // offset is a list where to pick random numbers from
   var inc = 0.005; // scope of different random numbers
 
-  this.show = function(){ // defining an object
-    stroke(random(100, 255));
+ this.show = function(){ // defining an object
+    stroke(random(100, 255)); // color of the shape line
     noFill();
-    strokeWeight(this.w);
+    strokeWeight(this.w); // size of the shape line
 
     beginShape();
     for (var i = 0 ; i < width; i ++) { // the origins of the lines are random but inside the width of the canvas
       var noiseVal = noise(offset) * this.y; // the trajectory of the lines
-      vertex(i, noiseVal); //lines between points
+      vertex(i, noiseVal); //place points and then draw a line between them. (btw : an elipse is a round shape)
       offset += inc; // the random trajectories won't vary too much from one another
     } 
     endShape();
+
+    // if you want to see points seperately : 
+    //  do ellise instead of vertex
+    //  no stroke
+    //  less points = for loops : till +=10 (instead of the width of 400)
+    //  less variance = offset increment : 0 is smooth, 1 is hectic 
+
+    // perlin noise : just use noise, similar to random
+    // random (), random (100), etc
+
+    //  var offset = random(1000)
+
   }
 }
